dafcd3c36428da4663c8b22647eddf2abca27b14
Onkardale <onkardale45@gmail.com>
2026-06-17 08:25:04 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+
+
+
+
+
+	}
+	@GetMapping("/demoProject")
+	public String MyName(){
+		return "hello";
+	}
+
+
+
+
+}
